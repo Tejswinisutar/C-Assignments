bfc554665fe3064be91599545d519def21d3cd7f
Tejaswini Sutar <tejswinisutar56@gmail.com>
2025-08-15 09:55:05 +0530

    Function Program

---
+++ Assignment On Function/01_Write a program that defines a function to print a welcome message..c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Welcome();    //Declaration
+
+int main()
+{
+    Welcome();         //Function Call
+    return 0;
+}
+
+void Welcome()
+{
+    printf("Welcome to Function Program..!\n");    //Defination
+}
